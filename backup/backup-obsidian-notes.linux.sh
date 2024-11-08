6ed9f3bd16a59fd5479dfdf477b4248acab57c8a
Brendan Murty <brendan@murty.au>
2024-11-08 12:17:25 +1100

    Improve backup script for Linux

--- backup/backup-obsidian-notes.linux.sh
+++ backup/backup-obsidian-notes.linux.sh
@@ -2,6 +2,7 @@
 
 # Backup Obsidian Notes
 #  - Makes a timestamped ZIP file backup of your entire Obsidian notes directory
+#  - Saves output messages to the file set in LOG_FILE
 #  - Requires zip, install with: sudo apt install zip
 #  - Requires a Linux system
 #  - Tested on Ubuntu 24.04, may need some tweaks for other distributions
@@ -13,24 +14,23 @@ BACKUP_FILE=obsidian_$(date +%Y%m%d-%H%M%S).zip
 
 BACKUP_PATH=$BACKUP_DIR/$BACKUP_FILE
 
-# Setup the message colour characters
+SCRIPT_PATH="$(dirname "$(readlink -f "$0")")"
 
-blue="\033[0;34m"
-green="\033[0;32m"
-yellow="\033[0;33m"
-red="\033[0;31m"
-end="\033[0m"
+LOG_FILE=$SCRIPT_PATH/backup-obsidian.log
+touch $LOG_FILE
+
+LOG_DATE_FORMAT="+%Y-%m-%d %H:%M:%S"
 
 # Exit if the source directory doesn't exist
 
 if [ ! -d $SOURCE_DIR ]; then
-  echo -e "${red}Error - Notes source directory ($SOURCE_DIR) not found, please check the path in the script${end}"
+  echo $(date "$LOG_DATE_FORMAT") "Error - Notes source directory ($SOURCE_DIR) not found, please check the path in the script" >> $LOG_FILE
   exit 1
 fi
 
 # Start backup process
 
-echo -e "${blue}Starting backup of your Notes in '$SOURCE_DIR' to '$BACKUP_PATH'...${end}"
+echo $(date "$LOG_DATE_FORMAT") "Starting backup of your Notes in '$SOURCE_DIR' to '$BACKUP_PATH'..." >> $LOG_FILE
 
 mkdir -p $BACKUP_DIR
 cd $SOURCE_DIR
@@ -39,8 +39,8 @@ zip -qr $BACKUP_PATH .
 # Done, print the relevant message
 
 if [[ $? -eq 0 ]]; then
-  echo -e "${green}Completed - Backup saved to '$BACKUP_PATH'.${end}"
+  echo $(date "$LOG_DATE_FORMAT") "Completed - Backup saved to '$BACKUP_PATH'." >> $LOG_FILE
 else
-  echo -e "${red}Error - Backup failed, please check output above.${end}"
+  echo $(date "$LOG_DATE_FORMAT") "Error - Backup failed, please check output above." >> $LOG_FILE
 fi
 
